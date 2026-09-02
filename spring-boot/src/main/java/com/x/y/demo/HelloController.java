12adc32a1463aca6342aa88bd8f6091d2c43bc5c
songjiabin <1796254117@qq.com>
2026-09-02 19:21:54 +0800

    ✨ [main] feat(spring-boot): 完善应用启动与可执行打包配置

---
+++ spring-boot/src/main/java/com/x/y/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.x.y.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class HelloController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "SUCCESS";
+    }
+}
