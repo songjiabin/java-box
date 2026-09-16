3b82fbe424b421df8ccdda88dbc720e47c08b4ef
songjiabin <1796254117@qq.com>
2026-09-16 19:26:17 +0800

    ✨ [main] feat(mybatis): 新增用户增删改查接口并修复 id 路径变量
    - 新增 queryUserById、addUser、updateUser、deleteUser 四个接口
    - 修复 @GetMapping 缺少 {id} 占位符导致 /queryUserById/2 返回 404
    - 补充 druid 模块 .gitignore，同步 .idea 编码配置

--- sptring-boot-my-batis/src/main/java/com/demo/spring/boot/my/batis/controller/UserController.java
+++ sptring-boot-my-batis/src/main/java/com/demo/spring/boot/my/batis/controller/UserController.java
@@ -4,13 +4,18 @@ package com.demo.spring.boot.my.batis.controller;
 import com.demo.spring.boot.my.batis.bean.User;
 import com.demo.spring.boot.my.batis.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class UserController {
+
+
     @Autowired
     private UserMapper userMapper;
 
@@ -24,5 +29,32 @@ public class UserController {
         return usersList;
     }
 
+    @GetMapping("/queryUserById/{id}")
+    public User queryUserById(@PathVariable(value = "id") int id) {
+        System.out.println("id====>" + id);
+        User user = userMapper.queryUserById(id);
+        return user;
+    }
+
+
+    @GetMapping("/addUser")
+    public String addUser() {
+        int i = userMapper.addUser(new User(8, "阿毛", "123456"));
+        return "ok";
+    }
+
+
+    @GetMapping("/updateUser")
+    public String updateUser() {
+        userMapper.updateUser(new User(8, "阿毛更新", "123455676"));
+        return "ok";
+    }
+
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable(value = "id") int id) {
+        int i = userMapper.deleteUser(id);
+        return "ok";
+    }
 
 }
